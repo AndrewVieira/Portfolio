d2190a2f2425e0e8506514b4dc773f23b9149e7a
Andrew Vieira <andrewkvieira@gmail.com>
2022-01-29 21:20:49 -0600

    Improved upon contact page styling

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,36 +1,38 @@
 import React from 'react';
 import { useForm, ValidationError } from '@formspree/react';
-import { Form, Button } from 'react-bootstrap';
+import { Stack, Form, Button } from 'react-bootstrap';
 
 function ContactForm () {
   const [state, handleSubmit] = useForm('mqknenay');
   if (state.succeeded) {
-    return <p>Thanks you!</p>;
+    return <h1 className="submit-success item-fadein">Thank You!</h1>;
   }
   return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group>
-        <Form.Label htmlFor="name">
-          Your Name
-        </Form.Label>
-        <Form.Control id="name" type="text" name="name" />
-        <ValidationError prefix="Name" field="name" errors={state.errors}/>
-      </Form.Group>
-      <Form.Group>
-        <Form.Label htmlFor="email">
-          Your Email
-        </Form.Label>
-        <Form.Control id="email" type="email" name="email" />
-        <ValidationError prefix="Email" field="email" errors={state.errors}/>
-      </Form.Group>
-      <Form.Group>
-        <Form.Label>Message</Form.Label>
-        <Form.Control id="message" name="message" as="textarea" rows={3} />
-        <ValidationError prefix="Message" field="message" errors={state.errors} />
-      </Form.Group>
-      <Button className="w-100" size="lg" type="submit" disabled={state.submitting}>
-        Submit
-      </Button>
+    <Form className="item-fadeout" onSubmit={handleSubmit}>
+      <Stack gap={1}>
+        <Form.Group>
+          <Form.Label htmlFor="name">
+            Your Name
+          </Form.Label>
+          <Form.Control id="name" type="text" name="name" />
+          <ValidationError prefix="Name" field="name" errors={state.errors}/>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label htmlFor="email">
+            Your Email
+          </Form.Label>
+          <Form.Control id="email" type="email" name="email" />
+          <ValidationError prefix="Email" field="email" errors={state.errors}/>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Message</Form.Label>
+          <Form.Control id="message" name="message" as="textarea" rows={3} />
+          <ValidationError prefix="Message" field="message" errors={state.errors} />
+        </Form.Group>
+        <Button className="w-100" size="lg" type="submit" disabled={state.submitting}>
+          Submit
+        </Button>
+      </Stack>
     </Form>
   );
 }
--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Row, Container, Col } from 'react-bootstrap';
 import ContactForm from '../components/ContactForm';
 
 function ContactPage () {
   return (
     <div className="ContactPage">
       <Container>
-        <h1>Contact Form</h1>
-        <ContactForm/>
+        <h1 className="mx-auto">Contact Form</h1>
+        <Row>
+          <Col>
+          </Col>
+          <Col xs={9} sm={7} md={5}>
+            <ContactForm/>
+          </Col>
+          <Col>
+          </Col>
+        </Row>
       </Container>
     </div>
   );
