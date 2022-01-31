4cd5e9c60238111539c9e623ce1eff0b029bb1c9
Andrew Vieira <andrewkvieira@gmail.com>
2022-01-31 16:15:23 -0600

    Improved Nav.Link Styling
    Changed Nav.Link styling to have bigger white text, and added a highlight on hover.

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -6,7 +6,7 @@ function AboutPage () {
     <div className="AboutPage">
       <Container>
         <Image className="mt-5" src="undraw_android_jr64.svg" style={{ height: '50vh' }}/>
-        <h2>Android and Web Developer</h2>
+        <h2>Developer</h2>
         <p>Lorem ipsum dolor sit amet, duo modus tantas ea, numquam appetere vis cu. Reque soleat accusata mea ea, cum enim civibus repudiandae eu. Electram sapientem mel cu, quem eruditi accusata at his, diam nibh facilisi ut sea. Vix graece eripuit oportere at, eam ei utamur iudicabit expetendis, te magna ullamcorper sed. Nec cu graece mandamus vituperata, ius at euismod tacimates, mel ut modo iriure epicurei.
 
 Id dicam iuvaret quaerendum qui. Nec etiam delenit petentium ea. His in populo corpora, ius omnis erant iisque eu. Vidit verterem vim cu. Pro eu harum mandamus. Erat veritus verterem ut cum.</p>
